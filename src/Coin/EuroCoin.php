37bebba72b68eb1d573f1cd3cd6ec44a484da248
Moh. Alkharraz <mohammad.alkharraz@7awi.com>
2022-07-29 16:17:22 +0300

    No need for sorting

--- src/Coin/EuroCoin.php
+++ src/Coin/EuroCoin.php
@@ -10,21 +10,11 @@ class EuroCoin implements CoinInterface
      */
     public function getCoinCombination(): array
     {
-        $coinCombination = [
-            1.0, 2.0,
-            0.01, 0.02, 0.05,
-            0.1, 0.2, 0.5,
+        return [
+            2.0,1.0,
+            0.05,0.02,0.01,
+            0.5,0.2,0.1
         ];
 
-        usort(
-            $coinCombination,
-            static function (float $a, float $b) {
-                $result = bccomp((string)$a, (string)$b, 2);
-
-                return $result >= 0 ? -1 : 1;
-            }
-        );
-
-        return $coinCombination;
     }
 }
